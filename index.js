6e0149c56e1df0880717feec630bd0803ba3cacb
Anurag Bhardwaj <anuragmac@Anurags-MacBook-Air.local>
2024-10-10 23:00:59 +0400

    master

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ mongoose
   .connect(`${process.env.MONGO_DB_URI}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
+  }) 
   .then(() => {
     console.log("Connected to MongoDB");
   })
